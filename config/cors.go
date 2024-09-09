976548949246f462201ea1d9520b8794ecff2d07
nekowawolf <irukainu20@gmail.com>
2024-09-09 21:05:57 +0700

    update: url

--- config/cors.go
+++ config/cors.go
@@ -16,6 +16,8 @@ var origins = []string{
 	"https://dias.ulbi.ac.id",
 	"https://iteung.ulbi.ac.id",
 	"https://whatsauth.github.io",
+	"http://nekowawolf.xyz",
+	"https://nekowawolf.github.io",
 }
 
 var Internalhost string = os.Getenv("INTERNALHOST") + ":" + os.Getenv("PORT")
