7aafe4bd18f426d94a8961e544bdc56fd3e2adf5
nekowawolf <irukainu20@gmail.com>
2024-09-11 04:11:16 +0700

    update: url cors

--- config/cors.go
+++ config/cors.go
@@ -8,15 +8,7 @@ import (
 )
 
 var origins = []string{
-	"https://auth.ulbi.ac.id",
-	"https://sip.ulbi.ac.id",
-	"https://euis.ulbi.ac.id",
-	"https://home.ulbi.ac.id",
-	"https://alpha.ulbi.ac.id",
-	"https://dias.ulbi.ac.id",
-	"https://iteung.ulbi.ac.id",
-	"https://whatsauth.github.io",
-	"https://nekowawolf.xyz/",
+	"https://nekowawolf.xyz",
 	"https://nekowawolf.github.io",
 }
 
